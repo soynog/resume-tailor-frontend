38dac3840bd67faf4dcdff3b533b7874ea8920cd
soynog <nicolas.o.garcia@gmail.com>
2016-04-27 19:16:15 -0400

    Proof of Concept for JSON Display
    - Hard-code example document json with titles and nodes and subnodes
    - Display in hierarchical format using handlebars

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,35 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+let jsonExample = {
+  "title": "Look at Me I'm a JSON",
+  "nodes": [  {"title": "I'm a node!",
+                "nodes": [{"title": "content content content"},
+                          {"title": "woo content"},
+                          {"title": "sooo much content"}]
+              },
+              {"title": "Me too!",
+               "nodes": [{"title": "content content content"},
+                         {"title": "woo content"},
+                         {"title": "sooo much content"}]
+              },
+              {"title": "Just another node...",
+               "nodes": [ {"title": "content content content"},
+                          {"title": "woo content"},
+                          {"title": "sooo much content"}]
+              }
+          ]
+};
+
+
+let renderJSON = function(){
+  let titleTemplate = require('./templates/doc-title.handlebars');
+  $('.content').append(titleTemplate({
+    jsonExample
+  }));
+};
+
+$(document).ready(function(){
+  renderJSON();
+});
